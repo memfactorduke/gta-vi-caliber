7097b5644d9101bb0164520db61b44e3314c0665
jakeefr <jakemwernerz@gmail.com>
2026-06-15 12:42:14 -0500

    chore: UE5 baseline project

---
+++ Source/GTC_UE5/GTC_UE5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GTC_UE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GTC_UE5, "GTC_UE5" );
---
+++ Source/GTC_UE5Editor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GTC_UE5EditorTarget : TargetRules
+{
+	public GTC_UE5EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("GTC_UE5");
+	}
+}
